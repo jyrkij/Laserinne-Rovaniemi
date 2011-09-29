c75fe9bbcfdd6841f482de3ce8cb04fa754908d7
Jyrki Lilja <jyrkij.lilja@gmail.com>
2011-09-29 12:13:03 +0300

    WIP NameDraw.

--- src/com/laserinne/namedraw/NameDraw.java
+++ src/com/laserinne/namedraw/NameDraw.java
@@ -23,91 +23,133 @@
 
 package com.laserinne.namedraw;
 
-import processing.core.*;
-import geomerative.*;
-import laserschein.*;
+import processing.core.PApplet;
 
+import com.laserinne.util.LaserinneSketch;
+import com.laserinne.util.Tracking;
+
+/**
+ * 
+ * @author Eero Ervast and Jyrki Lilja
+ */
 
 @SuppressWarnings("serial")
-public class NameDraw extends PApplet {
-    
-    Laserschein laser;
-
-//  PFont f;  // Global font variable
-    float x;  // horizontal location of headline
-    float y;
-    boolean overLine;
-    int index = 0;
-    float easing = 0.05f;
-    float targetX, targetY;
-    RFont font;
-
-    String[] nameList = {
-            "TANJA POUTIAINEN", "ANJA PAERSSON", "IHME HIIHTÄJÄs", "HIIHTO PUMMI", "SUKSI SUOHON", "HAVUJA SAATANA"
+public class NameDraw extends LaserinneSketch {
+    protected Spring[] spring = new Spring[1]; //I've deleted int num = 1; above so the number of the springs is assigned here without a separate command.
+    protected float x;
+    protected float y;
+    protected boolean overLine;
+    protected int index = 0;
+    protected String[] nameList = {
+            "TANJA POUTIAINEN", "ANJA PAERSSON", "IHME HIIHT€J€", "HIIHTO PUMMI", "SUKSI SUOHON", "HAVUJA SAATANA"
     };
-
-    public void setup() {
-        size(600, 600, P3D);
-        frameRate(-1);
-        overLine = false;
-        laser = new Laserschein(this, Laserschein.EASYLASEUSB2);
-        RG.init(this);
-        font = new RFont( "Laserfont.ttf", 14, RFont.CENTER);
+    
+    /**
+     * main
+     * 
+     * This has to be commented out when running in Processing to allow font
+     * loading, but available when running in Eclipse or building with Ant /
+     * exporting to application from Processing or building with Ant.
+     */
+    public static void main(String args[]) {
+         PApplet.main(new String[] { com.laserinne.namedraw.NameDraw.class.getName() });
     }
-
+    
+    public void setup() {
+        super.setup();
+        tracking = new Tracking();
+        overLine = false;
+        spring[0] = new Spring(70.0f, 160.0f, 20, 0.98f, 8.0f, 0.1f);
+    }
+    
     public void draw() {
-        background(0);
-        stroke(255);
+        super.draw();
+        
         line(0, 500, width, 500);
-
-        
-        // Easing
-        targetX = mouseX;
-        float dx = targetX - x;
-        if(abs(dx) > 1)  {
-            x += dx * easing;
-        }
-
-        targetY = mouseY;
-        float dy = targetY - y;
-        if(abs(dy) > 1) {
-            y += dy * easing;
-        } 
-
-        if (mouseY > 500) {
-            noStroke();
-        }  else {
-            stroke(255);
+    }
+    
+    protected void drawWithLaser() {
+        for(int i = 0; i < 1; i++)  {
+            spring[i].update();
+            spring[i].follow();
         }
         
-        Laser3D renderer = laser.renderer();
-        beginRaw(renderer);
-        noFill();
-        translate(x, y);
-        font.draw(nameList[index]);
-        endRaw();
-        
-        if (mouseY > 500 && overLine == false) {  
+        if (mouseY > 500 && overLine == false) {
             index += 1;
             overLine = true;
             if (index >= 6) {
                 index = 0;
             }
         }
-        if (mouseY <= 500 && overLine == true) {  
+        
+        if (mouseY <= 500 && overLine == true) {
             overLine = false;
         }
+        
+        if (mouseY > 500) {
+            noStroke();
+        } else {
+            stroke(NameDraw.LASER_COLOR);
+        }
+        
+        beginRaw(laserRenderer);
+        stroke(NameDraw.LASER_COLOR);
+        noFill();
+        font.draw(nameList[index]);
+        endRaw();
     }
     
-    public static void main(String args[]) {
-        PApplet.main(new String[] { NameDraw.class.getCanonicalName() });
+    class Spring {
+        // Screen values
+        float xpos, ypos;
+        float tempxpos, tempypos;
+        boolean move = true;
+        
+        // Spring simulation constants
+        float mass;       // Mass
+        float k = 0.2f;   // Spring constant
+        float damp;       // Damping
+        float rest_posx;  // Rest position X
+        float rest_posy;  // Rest position Y
+        
+        // Spring simulation variables
+        //float pos = 20.0; // Position
+        float velx = 0.0f;  // X Velocity
+        float vely = 0.0f;  // Y Velocity
+        float accel = 0;    // Acceleration
+        float force = 0;    // Force
+        
+        // Constructor
+        Spring (float x, float y, int s, float d, float m, float k_in) {
+            xpos = tempxpos = x;
+            ypos = tempypos = y;
+            rest_posx = x;
+            rest_posy = y;
+            damp = d;
+            mass = m;
+            k = k_in;
+        }
+        
+        void update() {
+            if (move) {
+                rest_posy = mouseY;
+                rest_posx = mouseX;
+            }
+            
+            force = -k * (tempypos - rest_posy);  // f=-ky
+            accel = force / mass;                 // Set the acceleration, f=ma == a=f/m
+            vely = damp * (vely + accel);         // Set the velocity
+            tempypos = tempypos + vely;           // Updated position
+            
+            force = -k * (tempxpos - rest_posx);  // f=-ky
+            accel = force / mass;                 // Set the acceleration, f=ma == a=f/m
+            velx = damp * (velx + accel);         // Set the velocity
+            tempxpos = tempxpos + velx;           // Updated position
+        }
+        
+        void follow() {
+            translate(tempxpos, tempypos);
+            move = true;
+        }
     }
 }
-
-/*void mouseClicked() {
- println(index);
- index += 1;
- if (x >= 6) {
- index = 0;
- }
- } */
